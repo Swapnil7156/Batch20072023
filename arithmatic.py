efb0d0c125cabdcb5e5699778a126431d174ec52
Swapnil7156 <swapnilshinde7156@gmail.com>
2023-07-21 00:10:32 +0530

    Done

--- arithmatic.py
+++ arithmatic.py
@@ -4,6 +4,10 @@ def addition(a,b):
     print(ans)
     return ans
 
+def subtraction(a,b):
+    ans=a-b
+    return ans
 a=10
 b=12
 print(f"The sum of a and b is {addition(a,b)}")
+print(f"The sub of a and b is {subtraction(a,b)}")
